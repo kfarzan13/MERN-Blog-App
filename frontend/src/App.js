73b21164b93ad3419d4bd1c3582e94d5a4894ded
Farzan Khan <kfarzan13@gmail.com>
2023-05-29 15:57:41 +0530

    Homepage, Login and Register and create Blog functionality added

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React  from 'react';
+import Header from './components/Header';
+import Post from './components/Post';
+import {Route, Routes} from 'react-router-dom';
+import Layout from './components/Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import {UserContextProvider} from './UserContext';
+import CreatePost from './pages/CreatePost';
+
+function App() {
+  return (
+    <UserContextProvider>
+        <Routes>
+          <Route path='/' element= {<Layout />}>
+            <Route index element={<IndexPage />} />
+            <Route path={'/login'} element={<LoginPage />} />
+            <Route path={'/register'} element={<RegisterPage />} />
+            <Route path={'/create'} element={<CreatePost />} />
+         </Route>  
+        </Routes>
+    </UserContextProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Post.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {formatISO9075} from 'date-fns';
+
+export default function Post ({title, summary, cover, content, createdAt}) {
+  return (
+    <div className='post'>
+        <div className='image'>
+          <img src='https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1' alt='' />
+        </div>
+        <div className='texts'>
+          <h2>{title}</h2>
+          <p className='info'>
+            <span className='author'>Farzan Khan</span>
+            <time>{formatISO9075(new Date(createdAt))}</time>
+          </p>
+          <p className='summary'>{summary}</p>
+        </div>
+      </div>
+  )
+}
